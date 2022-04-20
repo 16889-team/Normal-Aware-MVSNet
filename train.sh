74c1b57c8ade47db5f7b19034692301aef36d807
dijing <dijingzhang.1998@gmail.com>
2022-04-20 14:12:56 -0400

    modify

--- train.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-MVS_TRAINING="datasets/MVS_dataset/eth3d"
-python train.py --dataset=dtu_yao --batch_size=1 --trainpath=$MVS_TRAINING --trainlist lists/dtu/train.txt --testlist lists/dtu/test.txt --numdepth=192 --logdir ./checkpoints/wtf $@
